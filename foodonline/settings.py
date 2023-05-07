18e3719b786a65796cb89a4e06c7f6d2b66b0955
sazzadnibir <sazzadnibir@hotmail.com>
2023-05-07 15:50:19 +0600

    adds html templates

--- foodonline/settings.py
+++ foodonline/settings.py
@@ -117,6 +117,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / 'static_files'
+STATICFILES_DIRS = [
+    BASE_DIR / 'foodonline/static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
