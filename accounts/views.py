4284459c0e91cbee4c6f64428c6dda52775b4d83
sazzadnibir <sazzadnibir@hotmail.com>
2023-05-15 21:48:12 +0600

    adds form validation

--- accounts/views.py
+++ accounts/views.py
@@ -6,15 +6,35 @@ from .models import User
 
 def register_user(request):
     if request.method == 'POST':
-        # print(request.POST)
         form = UserForm(request.POST)
 
         if form.is_valid():
-            user = form.save(commit=False)
+            # password = form.cleaned_data["password"]
+
+            # user = form.save(commit=False)
+            # user.set_password(password)
+            # user.role = User.CUSTOMER
+            # user.save()
+            
+            ### Create the user using create_user method
+            first_name = form.cleaned_data["first_name"]
+            last_name = form.cleaned_data["last_name"]
+            username = form.cleaned_data["username"]
+            email = form.cleaned_data["email"]
+            password = form.cleaned_data["password"]
+            user = User.objects.create_user(
+                first_name=first_name,
+                last_name=last_name,
+                username=username,
+                email=email,
+                password=password
+            )
             user.role = User.CUSTOMER
             user.save()
-            
             return redirect("register-user")
+        else:
+            print("invalid form")
+            print(form.errors)
     else:
         form = UserForm()
 
